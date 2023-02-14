846edec830525d29c9947b5fcf4764ff596fb315
Quinn Gavin <54292238+quinn280@users.noreply.github.com>
2023-02-13 23:57:06 -0800

    create django project template

---
+++ RAdjangoproject/RAdjangoproject/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Room arranger home page test')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home),
+]
