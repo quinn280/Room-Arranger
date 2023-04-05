569a23c20745157b6f8c093a2db6a4fe0653f586
cmkoschnick <cmkoschnick@gmail.com>
2023-04-05 12:08:28 -0700

    Added actual links for searches

--- radjangoproject/api/furn_rec.py
+++ radjangoproject/api/furn_rec.py
@@ -1,10 +1,22 @@
 def returnSomeStuff(furniture_data):
     width = furniture_data['width']
     print(f"The width is {width}")
+    
+    wayfair_search = "https://www.wayfair.com/keyword.php?keyword="
+    ikea_search = "https://www.ikea.com/us/en/search/?q="
+    
+    amazon_search = "https://www.amazon.com/s?k="
+
+    search = furniture_data['description'] + " " + furniture_data['width'] + " by " + furniture_data['height']
+
+    amazon_search = amazon_search + search.replace(' ', '+')
+    ikea_search = ikea_search + search.replace(' ', '%20')
+    wayfair_search = wayfair_search + search.replace(' ', '+')
+
 
     links = {}
-    links['amazon'] = 'amazon.com'
-    links['ikea'] = 'ikea.com'
-    links['wayfair'] = 'wayfair.com'
+    links['amazon'] = amazon_search
+    links['ikea'] = ikea_search
+    links['wayfair'] = wayfair_search
 
     return links
