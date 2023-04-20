5dcf3b3a5c5f7a9136f479fe60b556bc1c69beca
AIDAN73 <aidangmurphree@gmail.com>
2023-04-19 18:37:16 -0700

    Fixed diplay issue, resized images

--- ra-react-app/src/Components/Editor/structureData.js
+++ ra-react-app/src/Components/Editor/structureData.js
@@ -19,12 +19,39 @@ const structureData = [
     },
     {
       itemKey: 52,
+      url: "vectors/structural/slidingdoor.png",
+      defaultWidth: 60,
+      defaultHeight: 4,
+      description: "Sliding Door",
+      category: "door",
+      type: "structural"
+    },
+    {
+      itemKey: 53,
       url: "vectors/structural/window.png",
       defaultWidth: 4,
       defaultHeight: 40,
       description: "Window",
       category: "window",
       type: "structural"
+    },
+    {
+      itemKey: 54,
+      url: "vectors/structural/ceilingfan.png",
+      defaultWidth: 48,
+      defaultHeight: 48,
+      description: "Ceiling Fan",
+      category: "Ceiling Fan",
+      type: "structural",
+    },
+    {
+      itemKey: 55,
+      url: "vectors/structural/fireplace.png",
+      defaultWidth: 35,
+      defaultHeight: 15,
+      description: "Fireplace",
+      category: "Fireplace",
+      type: "structural",
     },
   ];
 
